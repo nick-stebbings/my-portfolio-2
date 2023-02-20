afede1bda1b10b1f8aa990748fec3814a276d0db
Nick Stebb <nickolarse@riseup.net>
2023-02-21 11:57:53 +1300

    fix carousel

--- src/components/Carousel.vue
+++ src/components/Carousel.vue
@@ -1,7 +1,7 @@
 <script setup>
 </script>
 <template>
-    <div id="case-study-carousel" class="carousel slide" data-bs-interval="false" data-bs-ride="carousel">
+    <div id="case-study-carousel" class="carousel slide" data-bs-interval="false" data-bs-ride="true">
     <div class="carousel-indicators">
         <button type="button" data-bs-target="#case-study-carousel" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
         <button type="button" data-bs-target="#case-study-carousel" data-bs-slide-to="1" aria-label="Slide 2"></button>
